4cbc4d055f4bda3bae8bca282544a5ed887433c9
Kavyashree CR <kash@143130@gmail.com>
2024-07-22 22:12:39 +0530

    Completed assignment

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,14 @@
 */
 
 function isAnagram(str1, str2) {
-
-}
-
-module.exports = isAnagram;
+  
+    // Helper function to clean and sort the string
+    function cleanString(str) {
+      return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+    }
+  
+    return cleanString(str1) === cleanString(str2);
+  }
+  
+  module.exports = isAnagram;
+  
